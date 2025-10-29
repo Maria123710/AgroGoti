7b184223b9a4f600a41b6b5994c7c40406971156
Maria123710 <jannatultajrinmaria123@gmail.com>
2025-10-30 01:03:09 +0600

    done

--- python.py
+++ python.py
@@ -1,1 +1,42 @@
+# Simple calculator program in Python 
+'''Num1 = float(input("Enter first number: "))
+Num2 = float(input("Enter second number: "))
+Sum = Num1 + Num2
+Sub = Num1 - Num2
+Mul = Num1 * Num2
+Div = Num1 / Num2 
+print("The summation is:", Sum)
+print("The subtraction is:", Sub)
+print("The multiplication is:", Mul)
+print("The division is:", Div)
+
+import math
+
+redius = float(input("Enter the radius of the circle: "))
+Area = math.pi * redius * redius
+rounded_Area = round(Area, 2)
+
+print("The area of the circle is:", rounded_Area)
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a >= b and a >= c:
+    largest = a
+elif b >= a and b >= c:
+    largest = b
+else:
+    largest = c
+
+print("The largest number is:", largest)
+
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is an even number.") 
+else:
+    print(f"{number} is an odd number.")'''
+
 
